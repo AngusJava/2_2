d7c21c019f0dabe8edea103f1678608468aa4e6a
AngusJava <161606097+AngusJava@users.noreply.github.com>
2024-03-13 13:42:28 +0000

    2_2 Poprawione

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner liczba_a = new Scanner(System.in);
 
+    Scanner liczba_a = new Scanner(System.in);
     System.out.print("Twoja liczba A to  ");
     double a = liczba_a.nextDouble();
-    Scanner liczba_b = new Scanner(System.in);
 
+    Scanner liczba_b = new Scanner(System.in);
     System.out.print("Twoja liczba B to  ");
     double b = liczba_a.nextDouble();
-    Scanner liczba_c = new Scanner(System.in);
 
+    Scanner liczba_c = new Scanner(System.in);
     System.out.print("Twoja liczba C to  ");
     double c = liczba_a.nextDouble();
 
     double max = a;
     if (b > max) {
-      b = max;
+      max = b;
     }
     if (c > max) {
       max = c;
