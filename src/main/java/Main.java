e84c430b69a143392bb9685e94d5057697c82795
AngusJava <161606097+AngusJava@users.noreply.github.com>
2024-03-13 13:38:30 +0000

    2_2 koniec zadania

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner liczba_a = new Scanner(System.in);
+
+    System.out.print("Twoja liczba A to  ");
+    double a = liczba_a.nextDouble();
+    Scanner liczba_b = new Scanner(System.in);
+
+    System.out.print("Twoja liczba B to  ");
+    double b = liczba_a.nextDouble();
+    Scanner liczba_c = new Scanner(System.in);
+
+    System.out.print("Twoja liczba C to  ");
+    double c = liczba_a.nextDouble();
+
+    double max = a;
+    if (b > max) {
+      b = max;
+    }
+    if (c > max) {
+      max = c;
+    }
+    System.out.println("Największa liczba to " + max);
   }
 }
